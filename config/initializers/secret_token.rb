a75442611a1d76ac1242070651da7dfeca16de88
papanda <ichitanister@gmail.com>
2012-03-24 20:26:02 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WhiteShepherd::Application.config.secret_token = '6318a23e0b633ed62a449b621361d4697dd6f4d76c2d2c8ae69e95dbd2d012a7e80f46ee4f896d882bbfd9908dfebde3093fb2faa2b38740fb101765e4a787cf'
